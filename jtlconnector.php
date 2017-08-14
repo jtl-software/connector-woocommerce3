777d1cfed87d9f03b112b052a684fbe495851beb
Sven Mäurer <sven.maeurer@jtl-software.com>
2017-08-14 10:01:44 +0200

    Fix not updated version

--- jtlconnector.php
+++ jtlconnector.php
@@ -2,7 +2,8 @@
 /**
  * Plugin Name: JTL-Connector
  * Description: Verbinden Sie Ihren Shop mit JTL-Wawi, der kostenlosen Multichannel-Warenwirtschaft für den Versandhandel.
- * Version: 1.4.1
+ * Version: 1.4.2
+ *
  * Author: JTL-Software GmbH
  * Author URI: http://www.jtl-software.de
  * License: GPL3
@@ -34,7 +35,7 @@ if (rewriting_disabled()) {
     add_action('admin_notices', 'rewriting_not_activated');
 } else {
     define('CONNECTOR_DIR', __DIR__);
-    define('CONNECTOR_VERSION', '1.4.1');
+    define('CONNECTOR_VERSION', '1.4.2');
     define('DS', DIRECTORY_SEPARATOR);
     define('INCLUDES_DIR', plugin_dir_path(__FILE__) . 'includes' . DS);
 
