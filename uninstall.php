e48adde8597969c72d0152e3e8266af9d05e030e
Sven Mäurer <sven.maeurer@jtl-software.com>
2017-10-23 10:39:43 +0200

    Delete all options on uninstall

--- uninstall.php
+++ uninstall.php
@@ -6,12 +6,14 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 global $wpdb;
 
-$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_category');
-$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_crossselling');
-$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_customer');
-$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_image');
-$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_order');
-$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_payment');
-$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_product');
-$wpdb->query('DROP TABLE IF EXISTS jtl_connector_category_level');
-$wpdb->query('DROP TABLE IF EXISTS jtl_connector_product_checksum');
+$wpdb->query('DROP TABLE IF EXISTS `jtl_connector_link_category`');
+$wpdb->query('DROP TABLE IF EXISTS `jtl_connector_link_crossselling`');
+$wpdb->query('DROP TABLE IF EXISTS `jtl_connector_link_customer`');
+$wpdb->query('DROP TABLE IF EXISTS `jtl_connector_link_image`');
+$wpdb->query('DROP TABLE IF EXISTS `jtl_connector_link_order`');
+$wpdb->query('DROP TABLE IF EXISTS `jtl_connector_link_payment`');
+$wpdb->query('DROP TABLE IF EXISTS `jtl_connector_link_product`');
+$wpdb->query('DROP TABLE IF EXISTS `jtl_connector_category_level`');
+$wpdb->query('DROP TABLE IF EXISTS `jtl_connector_product_checksum`');
+
+$wpdb->query("DELETE FROM `{$wpdb->options}` WHERE `option_name` LIKE 'jtlconnector_%'");
