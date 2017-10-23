a10683b4ac81203c36d07e04cff54051a865b622
Sven Mäurer <sven.maeurer@jtl-software.com>
2017-10-23 10:26:14 +0200

    Drop connector tables on uninstall

--- uninstall.php
+++ uninstall.php
@@ -6,6 +6,12 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 global $wpdb;
 
-$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link');
+$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_category');
+$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_crossselling');
+$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_customer');
+$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_image');
+$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_order');
+$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_payment');
+$wpdb->query('DROP TABLE IF EXISTS jtl_connector_link_product');
 $wpdb->query('DROP TABLE IF EXISTS jtl_connector_category_level');
 $wpdb->query('DROP TABLE IF EXISTS jtl_connector_product_checksum');
