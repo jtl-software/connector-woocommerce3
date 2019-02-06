44c0b4a8321458620aae2b5f6ba388d481aabb1d
Jan Weskamp <jan.weskamp@jtl-software.com>
2019-02-06 17:41:07 +0100

    hotfix
    prevent issues of specific/specific value was set incorrectly

--- src/Controllers/Specific.php
+++ src/Controllers/Specific.php
@@ -77,137 +77,156 @@ class Specific extends BaseController
     {
         //WooFix
         $specific->setType('string');
-        $meta = null;
+        $meta            = null;
+        $defaultAvailable = false;
         
         foreach ($specific->getI18ns() as $i18n) {
-            if (Util::getInstance()->isWooCommerceLanguage($i18n->getLanguageISO())) {
+            $languageSet = Util::getInstance()->isWooCommerceLanguage($i18n->getLanguageISO());
+            
+            if (strcmp($i18n->getLanguageISO(), 'ger') === 0) {
+                $defaultAvailable = true;
+            }
+            
+            if ($languageSet) {
                 $meta = $i18n;
                 break;
             }
         }
         
-        $attrName = wc_sanitize_taxonomy_name(Util::removeSpecialchars($meta->getName()));
+        //Fallback if incorrect language code was given
+        if ($meta === null && $defaultAvailable) {
+            foreach ($specific->getI18ns() as $i18n) {
+                if (strcmp($i18n->getLanguageISO(), 'ger') === 0) {
+                    $meta->$i18n;
+                }
+            }
+        }
         
-        //STOP here if already exists
-        $exId  = Util::getAttributeTaxonomyIdByName($attrName);
-        $endId = (int)$specific->getId()->getEndpoint();
-        
-        if ($exId !== 0) {
-            if ($exId !== $endId) {
-                $attrId = $exId;
+        if ($meta !== null) {
+            
+            $attrName = wc_sanitize_taxonomy_name(Util::removeSpecialchars($meta->getName()));
+            
+            //STOP here if already exists
+            $exId  = Util::getAttributeTaxonomyIdByName($attrName);
+            $endId = (int)$specific->getId()->getEndpoint();
+            
+            if ($exId !== 0) {
+                if ($exId !== $endId) {
+                    $attrId = $exId;
+                } else {
+                    $attrId = $endId;
+                }
             } else {
                 $attrId = $endId;
             }
-        } else {
-            $attrId = $endId;
-        }
-        
-        $endpoint = [
-            'id'       => $attrId,
-            'name'     => $meta->getName(),
-            'slug'     => wc_sanitize_taxonomy_name(substr(trim($meta->getName()), 0, 27)),
-            'type'     => 'select',
-            'order_by' => 'menu_order',
-            //'attribute_public'  => 0,
-        ];
-        
-        if ($endpoint['id'] === 0) {
-            $attributeId = wc_create_attribute($endpoint);
-        } else {
-            $attributeId = wc_update_attribute($endpoint['id'], $endpoint);
-        }
-        
-        if ($attributeId instanceof WP_Error) {
-            //var_dump($attributeId);
-            //die();
-            //return $termId->get_error_message();
-            WpErrorLogger::getInstance()->logError($attributeId);
             
-            return $specific;
-            
-        }
-        
-        $specific->getId()->setEndpoint($attributeId);
-        
-        //Get taxonomy
-        $taxonomy = $attrName ?
-            'pa_' . wc_sanitize_taxonomy_name(substr(trim($meta->getName()), 0, 27))
-            : '';
-        
-        //Register taxonomy for current request
-        register_taxonomy($taxonomy, null);
-        
-        /** @var SpecificValueModel $value */
-        foreach ($specific->getValues() as $key => $value) {
-            $value->getSpecificId()->setEndpoint($attributeId);
-            $metaValue = null;
-            
-            //Get i18n
-            foreach ($value->getI18ns() as $i18n) {
-                if (Util::getInstance()->isWooCommerceLanguage($i18n->getLanguageISO())) {
-                    $metaValue = $i18n;
-                    break;
-                }
-            }
-            
-            if (is_null($metaValue)) {
-                continue;
-            }
-            
-            $slug = wc_sanitize_taxonomy_name($metaValue->getValue());
-            
-            $endpointValue = [
-                'name' => $metaValue->getValue(),
-                'slug' => $slug,
+            $endpoint = [
+                'id'       => $attrId,
+                'name'     => $meta->getName(),
+                'slug'     => wc_sanitize_taxonomy_name(substr(trim($meta->getName()), 0, 27)),
+                'type'     => 'select',
+                'order_by' => 'menu_order',
+                //'attribute_public'  => 0,
             ];
             
-            $exValId = $this->database->query(
-                SqlHelper::getSpecificValueId(
-                    $taxonomy,
-                    $endpointValue['name']
-                )
-            );
-            
-            if (count($exValId) >= 1) {
-                if (isset($exValId[0]['term_id'])) {
-                    $exValId = $exValId[0]['term_id'];
-                } else {
-                    $exValId = null;
-                }
+            if ($endpoint['id'] === 0) {
+                $attributeId = wc_create_attribute($endpoint);
             } else {
-                $exValId = null;
+                $attributeId = wc_update_attribute($endpoint['id'], $endpoint);
             }
             
-            $endValId = (int)$value->getId()->getEndpoint();
-            
-            if (is_null($exValId) && $endValId === 0) {
-                $newTerm = \wp_insert_term(
-                    $endpointValue['name'],
-                    $taxonomy
-                );
+            if ($attributeId instanceof WP_Error) {
+                //var_dump($attributeId);
+                //die();
+                //return $termId->get_error_message();
+                WpErrorLogger::getInstance()->logError($attributeId);
                 
-                if ($newTerm instanceof WP_Error) {
-                    //  var_dump($newTerm);
-                    // die();
-                    WpErrorLogger::getInstance()->logError($newTerm);
+                return $specific;
+                
+            }
+            
+            $specific->getId()->setEndpoint($attributeId);
+            
+            //Get taxonomy
+            $taxonomy = $attrName ?
+                'pa_' . wc_sanitize_taxonomy_name(substr(trim($meta->getName()), 0, 27))
+                : '';
+            
+            //Register taxonomy for current request
+            register_taxonomy($taxonomy, null);
+            
+            /** @var SpecificValueModel $value */
+            foreach ($specific->getValues() as $key => $value) {
+                $value->getSpecificId()->setEndpoint($attributeId);
+                $metaValue = null;
+                
+                //Get i18n
+                foreach ($value->getI18ns() as $i18n) {
+                    if (Util::getInstance()->isWooCommerceLanguage($i18n->getLanguageISO())) {
+                        $metaValue = $i18n;
+                        break;
+                    }
+                }
+                
+                if (is_null($metaValue)) {
                     continue;
                 }
                 
-                $termId = $newTerm['term_id'];
-            } elseif (is_null($exValId) && $endValId !== 0) {
-                $termId = \wp_update_term($endValId, $taxonomy, $endpointValue);
-            } else {
-                $termId = $exValId;
+                $slug = wc_sanitize_taxonomy_name($metaValue->getValue());
+                
+                $endpointValue = [
+                    'name' => $metaValue->getValue(),
+                    'slug' => $slug,
+                ];
+                
+                $exValId = $this->database->query(
+                    SqlHelper::getSpecificValueId(
+                        $taxonomy,
+                        $endpointValue['name']
+                    )
+                );
+                
+                if (count($exValId) >= 1) {
+                    if (isset($exValId[0]['term_id'])) {
+                        $exValId = $exValId[0]['term_id'];
+                    } else {
+                        $exValId = null;
+                    }
+                } else {
+                    $exValId = null;
+                }
+                
+                $endValId = (int)$value->getId()->getEndpoint();
+                
+                if (is_null($exValId) && $endValId === 0) {
+                    $newTerm = \wp_insert_term(
+                        $endpointValue['name'],
+                        $taxonomy
+                    );
+                    
+                    if ($newTerm instanceof WP_Error) {
+                        //  var_dump($newTerm);
+                        // die();
+                        WpErrorLogger::getInstance()->logError($newTerm);
+                        continue;
+                    }
+                    
+                    $termId = $newTerm['term_id'];
+                } elseif (is_null($exValId) && $endValId !== 0) {
+                    $termId = \wp_update_term($endValId, $taxonomy, $endpointValue);
+                } else {
+                    $termId = $exValId;
+                }
+                
+                if ($termId instanceof WP_Error) {
+                    // var_dump($termId);
+                    // die();
+                    WpErrorLogger::getInstance()->logError($termId);
+                    continue;
+                }
+                
+                $value->getId()->setEndpoint($termId);
             }
-            
-            if ($termId instanceof WP_Error) {
-                // var_dump($termId);
-                // die();
-                WpErrorLogger::getInstance()->logError($termId);
-                continue;
-            }
-            
-            $value->getId()->setEndpoint($termId);
         }
         
         return $specific;
@@ -218,9 +237,9 @@ class Specific extends BaseController
         $specificId = (int)$specific->getId()->getEndpoint();
         
         if ( ! empty($specificId)) {
-    
+            
             unset(self::$idCache[$specific->getId()->getHost()]);
-    
+            
             $this->database->query(SqlHelper::removeSpecificLinking($specificId));
             $taxonomy = wc_attribute_taxonomy_name_by_id($specificId);
             /** @var \WC_Product_Attribute $specific */
@@ -230,14 +249,14 @@ class Specific extends BaseController
                 SqlHelper::forceSpecificValuePull($taxonomy)
             );
             
-            $terms             = [];
+            $terms = [];
             foreach ($specificValueData as $specificValue) {
                 $terms[] = $specificValue['slug'];
-    
+                
                 $this->database->query(SqlHelper::removeSpecificValueLinking($specificValue['term_id']));
             }
             
-            $products    = new WP_Query([
+            $products = new WP_Query([
                 'post_type'      => ['product'],
                 'posts_per_page' => -1,
                 'tax_query'      => [
@@ -265,8 +284,8 @@ class Specific extends BaseController
                     }
                 }
             }
-     
-            if (! $isVariation) {
+            
+            if ( ! $isVariation) {
                 
                 foreach ($specificValueData as $value) {
                     \wp_delete_term($value['term_id'], $taxonomy);
