21d4405d60c41a38dfc3db1a583a8bc1fffacce4
Patryk Gorol <patryk.gorol@jtl-software.com>
2020-04-29 13:51:13 +0200

    Added condition to payment pull query to get only linked orders Moved default attribute at the end

--- src/Controllers/Payment.php
+++ src/Controllers/Payment.php
@@ -24,7 +24,7 @@ class Payment extends BaseController
 
         $includeCompletedOrders = \get_option(\JtlConnectorAdmin::OPTIONS_COMPLETED_ORDERS, 'yes') === 'yes';
 
-        $completedOrders = $this->database->queryList(SqlHelper::paymentCompletedPull($limit, $includeCompletedOrders));
+        $completedOrders = $this->database->queryList(SqlHelper::paymentCompletedPull($includeCompletedOrders, $limit));
 
         foreach ($completedOrders as $orderId) {
             $order = \wc_get_order((int)$orderId);
@@ -64,6 +64,6 @@ class Payment extends BaseController
     {
         $includeCompletedOrders = \get_option(\JtlConnectorAdmin::OPTIONS_COMPLETED_ORDERS, 'yes') === 'yes';
 
-        return (int)$this->database->queryOne(SqlHelper::paymentCompletedPull(null, $includeCompletedOrders));
+        return (int)$this->database->queryOne(SqlHelper::paymentCompletedPull($includeCompletedOrders));
     }
 }
