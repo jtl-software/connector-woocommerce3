6c58b223c633e8aead4d9e66d2cd10e715aa5b49
Patryk Gorol <patryk.gorol@jtl-software.com>
2020-07-28 13:02:04 +0200

    Release info update

--- woo-jtl-connector.php
+++ woo-jtl-connector.php
@@ -2,9 +2,9 @@
 /**
  * Plugin Name: WooCommerce JTL-Connector
  * Description: Connect your woocommerce-shop with JTL-Wawi, the free multichannel-erp for mail order business.
- * Version: 1.9.5.2
+ * Version: 1.10.0
  * Requires PHP: 7.1.3
- * WC tested up to: 4.0
+ * WC tested up to: 4.1
  * Author: JTL-Software GmbH
  * Author URI: http://www.jtl-software.de
  * License: GPL3
