4e536ea19ef576a18c68f763fe34ba1434578372
Patryk Gorol <patryk.gorol@jtl-software.com>
2020-10-06 11:08:33 +0200

    Readme update bump version

--- includes/JtlConnectorAdmin.php
+++ includes/JtlConnectorAdmin.php
@@ -1948,6 +1948,7 @@ final class JtlConnectorAdmin
             case '1.12.0':
             case '1.13.0':
             case '1.13.1':
+            case '1.14.0':
             default:
                 self::activate_linking();
         }
