3a2e925723192fa5e00451f9673b669763ad958b
Patryk Gorol <patryk.gorol@jtl-software.com>
2021-01-11 14:39:02 +0100

    Updated features.json to handle invisibility Added support for B2B Market category/product blacklist Increased minimum php version to 7.2 Updated phpunit

---
+++ src/Utilities/B2BMarket.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace JtlWooCommerceConnector\Utilities;
+
+use jtl\Connector\Model\DataModel;
+use JtlWooCommerceConnector\Controllers\GlobalData\CustomerGroup;
+
+class B2BMarket
+{
+    /**
+     * @param array $customerGroupsIds
+     * @param string $metaKey
+     * @param DataModel ...$models
+     */
+    protected function setB2BCustomerGroupBlacklist(array $customerGroupsIds, string $metaKey, DataModel ...$models)
+    {
+        $newCustomerGroupBlacklist = [];
+        foreach ($models as $model) {
+            if (method_exists($model, 'getInvisibilities') && in_array($metaKey, ['bm_conditional_products', 'bm_conditional_categories'])) {
+                foreach ($model->getInvisibilities() as $modelInvisibility) {
+                    $customerGroupId = $modelInvisibility->getCustomerGroupId()->getEndpoint();
+                    if (!empty($customerGroupId) && in_array($customerGroupId, $customerGroupsIds, true)) {
+                        $newCustomerGroupBlacklist[$customerGroupId][] = $model->getId()->getEndpoint();
+                    }
+                }
+            }
+        }
+
+        foreach ($customerGroupsIds as $customerGroupId) {
+            $customerGroupId = (int)$customerGroupId;
+            $currentCustomerGroupBlackList = $this->getPostMeta($customerGroupId, $metaKey, true);
+            if (isset($newCustomerGroupBlacklist[$customerGroupId])) {
+                $this->updatePostMeta($customerGroupId, $metaKey, join(',', $newCustomerGroupBlacklist[$customerGroupId]), $currentCustomerGroupBlackList);
+            } else {
+                $this->deletePostMeta($customerGroupId, $metaKey, $currentCustomerGroupBlackList);
+            }
+        }
+    }
+
+    /**
+     * @param string $controller
+     * @param DataModel ...$entities
+     */
+    public function handleCustomerGroupsBlacklists(string $controller, DataModel ...$entities)
+    {
+        $customerGroups = (new CustomerGroup())->pullData();
+        $customerGroupsIds = array_values(array_map(function (\jtl\Connector\Model\CustomerGroup $customerGroup) {
+            return $customerGroup->getId()->getEndpoint();
+        }, $customerGroups));
+
+        $metaKey = '';
+        switch ($controller) {
+            case 'product':
+                $metaKey = 'bm_conditional_products';
+                break;
+            case 'category':
+                $metaKey = 'bm_conditional_categories';
+                break;
+        }
+
+        if (!empty($metaKey)) {
+            $this->setB2BCustomerGroupBlacklist($customerGroupsIds, $metaKey, ...$entities);
+        }
+    }
+}
---
+++ tests/src/Utilities/B2BMarket.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace src\Utilities;
+
+use jtl\Connector\Model\Identity;
+use jtl\Connector\Model\Product;
+use jtl\Connector\Model\ProductInvisibility;
+use JtlWooCommerceConnector\Tests\AbstractTestCase;
+
+/**
+ * Class B2BMarket
+ * @package src\Utilities
+ */
+class B2BMarket extends AbstractTestCase
+{
+    /**
+     * @throws \ReflectionException
+     */
+    public function testSetB2BCustomerGroupBlacklist()
+    {
+        $b2bMock = $this->getMockBuilder(\JtlWooCommerceConnector\Utilities\B2BMarket::class)
+            ->addMethods(['getPostMeta', 'updatePostMeta', 'deletePostMeta'])
+            ->getMock();
+
+        $b2bMock->expects($this->exactly(2))
+            ->method('getPostMeta')
+            ->withConsecutive(['10', 'bm_conditional_products'], ['11', 'bm_conditional_products'])
+            ->willReturnOnConsecutiveCalls([], []);
+
+        $b2bMock->expects($this->once())
+            ->method('deletePostMeta')
+            ->with('11', 'bm_conditional_products', []);
+
+        $b2bMock->expects($this->once())
+            ->method('updatePostMeta')
+            ->with('10', 'bm_conditional_products', join(',', ['1']), []);
+
+        $products = [
+            (new Product())->setId(new Identity("1", 1))->setInvisibilities([
+                (new ProductInvisibility())->setCustomerGroupId(new Identity("10", 1))
+            ])
+        ];
+
+        $reflection = new \ReflectionClass($b2bMock);
+        $method = $reflection->getMethod('setB2BCustomerGroupBlacklist');
+        $method->setAccessible(true);
+
+        $method->invoke($b2bMock, ['10', '11'], 'bm_conditional_products', ...$products);
+    }
+}
