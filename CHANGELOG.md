d5315e4ea67b42c431b06b2e2614f7338e4f37d6
Patrick Gugelsberger <pgugelsberger@gmail.com>
2023-02-02 12:00:34 +0100

    added changelog entry

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@ This is the changelog of our "JTL WooCommerce Connector".
 
 ## Unreleased
 - CO-2242 - added customer group special price support
+- CO-2263 - fix empty delivery addresses
 
 ## 1.39.4 _2022-12-13_
 - CO-2163 - fix extra product options plugin support
--- src/Controllers/Order/CustomerOrderAddress.php
+++ src/Controllers/Order/CustomerOrderAddress.php
@@ -35,19 +35,19 @@ class CustomerOrderAddress extends BaseController
 
     protected function createDefaultAddresses(DataModel $address): void
     {
-        if (strcmp($address->getCity(), '') === 0) {
+        if (\strcmp($address->getCity(), '') === 0) {
             $address->setCity(get_option('woocommerce_store_city'));
         }
 
-        if (strcmp($address->getZipCode(), '') === 0) {
+        if (\strcmp($address->getZipCode(), '') === 0) {
             $address->setZipCode(get_option('woocommerce_store_postcode'));
         }
 
-        if (strcmp($address->getStreet(), '') === 0) {
+        if (\strcmp($address->getStreet(), '') === 0) {
             $address->setStreet(get_option('woocommerce_store_address'));
         }
 
-        if (strcmp($address->getCountryIso(), '') === 0) {
+        if (\strcmp($address->getCountryIso(), '') === 0) {
             $address->setCountryIso(get_option('woocommerce_default_country'));
         }
     }
