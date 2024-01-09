957c12dc960b30c2ae325c3cedbbd0b0b2d6ab5c
Cem Aktas <cem.aktas@jtl-software.com>
2024-01-09 15:39:52 +0100

    add new controller

---
+++ src/Controllers/Product/ProductAdvancedCustomFieldsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace JtlWooCommerceConnector\Controllers\Product;
+
+use Jtl\Connector\Core\Model\Product as ProductModel;
+use JtlWooCommerceConnector\Controllers\AbstractBaseController;
+
+/**
+ * Class ProductAdvancedCustomFields
+ *
+ * @package JtlWooCommerceConnector\Controllers\Product
+ */
+class ProductAdvancedCustomFieldsController extends AbstractBaseController
+{
+    public function pullData()
+    {
+    }
+
+    public function pushData()
+    {
+    }
+}
