c711cfb2b90bd1e96077a8478a72945310112e54
Cem Aktas <cem.aktas@jtl-software.com>
2025-01-10 16:19:59 +0100

    update Faker

--- tests/src/Faker/DbFaker.php
+++ tests/src/Faker/DbFaker.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace src\Faker;
+declare(strict_types=1);
+
+namespace JtlWooCommerceConnector\Tests\Faker;
 
 use JtlWooCommerceConnector\Utilities\Db;
 
@@ -10,10 +12,16 @@ class DbFaker extends Db
      * @var string[]
      */
     public array $givenQueries = [];
+
+    /**
+     * @param string $query
+     * @param bool $shouldLog
+     * @return array<string, bool|int|string|null>|null Faker Database query results
+     * @throws \Psr\Log\InvalidArgumentException
+     */
     public function query(string $query, bool $shouldLog = true): ?array
     {
         $this->givenQueries[] = $query;
         return parent::query($query, $shouldLog);
     }
-
 }
