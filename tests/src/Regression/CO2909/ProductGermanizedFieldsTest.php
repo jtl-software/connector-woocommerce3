e1239988eed6503ecab9bf72dfaa4a4ffd787c94
Marius Brandt <marius.brandt@jtl-software.com>
2025-03-13 11:51:28 +0100

    fix error in wp_delete_object_term_relationships where it will explode given an unknown taxonomy

---
+++ tests/src/Regression/CO2909/ProductGermanizedFieldsTest.php
@@ -0,0 +1,86 @@
+<?php /** @noinspection PhpIllegalPsrClassPathInspection */
+declare(strict_types=1);
+
+namespace JtlWooCommerceConnector\Tests\Regression\CO2909 {
+
+    use Jtl\Connector\Core\Model\Product;
+    use JtlWooCommerceConnector\Controllers\Product\ProductGermanizedFieldsController;
+    use JtlWooCommerceConnector\Utilities\Db;
+    use JtlWooCommerceConnector\Utilities\Util;
+    use phpmock\Mock;
+    use phpmock\MockBuilder;
+    use phpmock\MockEnabledException;
+    use PHPUnit\Framework\TestCase;
+
+    class ProductGermanizedFieldsTest extends TestCase
+    {
+        protected Mock $getLocale;
+
+        /**
+         * @return void
+         * @throws \InvalidArgumentException
+         * @throws MockEnabledException
+         */
+        protected function setUp(): void
+        {
+            parent::setUp();
+            $this->getLocale = (new MockBuilder())->setNamespace('JtlWooCommerceConnector\Utilities')
+                ->setName('get_locale')
+                ->setFunction(
+                    function () {
+                        return 'de_DE';
+                    }
+                )->build();
+
+            $this->getLocale->enable();
+        }
+
+        /**
+         * @return void
+         */
+        protected function tearDown(): void
+        {
+            parent::tearDown();
+            $this->getLocale->disable();
+        }
+
+        /**
+         * @return void
+         * @throws \ReflectionException
+         */
+        public function testCanUpdateGermanizedGpsrData(): void
+        {
+            self::expectNotToPerformAssertions();
+            $product = new Product();
+
+            $db   = $this->getMockBuilder(Db::class)->disableOriginalConstructor()->getMock();
+            $util = $this->getMockBuilder(Util::class)->disableOriginalConstructor()->getMock();
+
+            $germanizedController = new ProductGermanizedFieldsController($db, $util);
+
+            $controller = new \ReflectionClass($germanizedController);
+            $update     = $controller->getMethod('updateGermanizedGpsrData');
+
+            $update->invoke($germanizedController, $product);
+        }
+    }
+}
+
+namespace {
+    class WP_Error {}
+
+    function wp_get_object_terms($object_ids, $taxonomies, $args = [])
+    {
+        return new \WP_Error();
+    }
+
+    function update_post_meta($post_id, $meta_key, $meta_value, $prev_value = '')
+    {
+        return true;
+    }
+
+    function wp_remove_object_terms($object_id, $terms, $taxonomy)
+    {
+        throw new \RuntimeException('This method should never be called');
+    }
+}
