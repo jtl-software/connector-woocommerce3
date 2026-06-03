ac25b07d6a8fd47c3d8fc70f4767317947d0ccc5
Patrick Gugelsberger <pgugelsberger@gmail.com>
2026-06-03 17:18:31 +0200

    fix: use inline phpcs:ignore to coexist with translators comment

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -751,9 +751,8 @@ class ProductController extends AbstractBaseController implements
         $productId = (int)$wcProduct->get_id();
 
         $productTitle = \esc_html(\get_the_title((int)$product->getMasterProductId()->getEndpoint()));
-        // phpcs:ignore WordPress.WP.I18n.TextDomainMismatch -- Third-party string from WooCommerce
         // translators: %1$s is the variation product ID, %2$s is the parent product title
-        $variation_post_title = \sprintf(\__('Variation #%1$s of %2$s', 'woocommerce'), $productId, $productTitle);
+        $variation_post_title = \sprintf(\__('Variation #%1$s of %2$s', 'woocommerce'), $productId, $productTitle); // phpcs:ignore WordPress.WP.I18n.TextDomainMismatch -- Third-party string from WooCommerce
         \wp_update_post([
             'ID' => $productId,
             'post_title' => $variation_post_title,
